010d70e21610e2c0d24f0c56cd2a4653ad38192b
Skirja <irzafinmuh11@gmail.com>
2024-10-07 16:10:46 +0700

    tugas

--- logout_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['user_id'])) {
-    echo "DEBUG: Still logged in according to logout_check.php. Session ID: " . session_id();
-} else {
-    echo "DEBUG: Logged out according to logout_check.php.";
-}
-?>
